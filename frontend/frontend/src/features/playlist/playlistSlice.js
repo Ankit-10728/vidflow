dd56e496d3c71670b592e4089150d111c598727d
Ankit-10728 <aankit21kr@gmail.com>
2026-02-24 22:49:47 +0530

    fix: update playlist handling in removeFromPlaylist and getPlaylistById, improve error handling

--- frontend/frontend/src/features/playlist/playlistSlice.js
+++ frontend/frontend/src/features/playlist/playlistSlice.js
@@ -41,11 +41,18 @@ const playlistSlice = createSlice({
 
             .addCase(removeFromPlaylist.fulfilled, (state, action) => {
                 state.loading = false;
+                const playlistId = action.payload.data._id;
+                state.playlists.myPlaylist = state.playlists.myPlaylist.map((p) => p._id == playlistId ? action.payload?.data : p);
             })
 
             .addCase(getPlaylistById.fulfilled, (state, action) => {
                 state.loading = false;
-                if (action.payload?.data) state.playlists.playlistUser.unshift(action.payload.data);
+                const playlist = action.payload.data;
+                const exists = state.playlists.playlistUser.find(p => p._id === playlist._id);
+
+                if (!exists) {
+                    state.playlists.playlistUser.push(playlist);
+                }
             })
 
             .addCase(deletePlaylist.fulfilled, (state, action) => {
@@ -62,7 +69,7 @@ const playlistSlice = createSlice({
 
             .addMatcher(isRejected(thunks), (state, action) => {
                 state.loading = false;
-                state.error = action.payload;
+                state.error = action.payload || action.error.message;
             });
     },
 });
