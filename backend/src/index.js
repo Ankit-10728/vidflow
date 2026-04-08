c87aa477ac8ef4c2b27423f82a93138ec425574f
Ankit-10728 <aankit21kr@gmail.com>
2026-04-08 18:17:02 +0530

    fix: correct VITE_API_URL in .env and update axios baseURL to use environment variable refactor: define PORT variable in index.js for better readability

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,8 +19,10 @@ console.log("SERVER FILE LOADED");
             throw error
         })
 
-        app.listen(process.env.PORT, () => {
-            console.log(`App is listening on port ${process.env.PORT}`);
+        const PORT = process.env.PORT || 8000;
+
+        app.listen(PORT, () => {
+            console.log(`App is listening on port ${PORT}`);
         })
 
     } catch (error) {
